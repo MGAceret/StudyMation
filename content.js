d672efcf63f6c897658831384f7ce78dc3786d37
Xyans Yes <mgaceret@gmail.com>
2026-09-06 23:13:05 +0800

    initialize container to display inside the player

--- content.js
+++ content.js
@@ -5,3 +5,18 @@ function getVideo() {
 }
 
 let myVideo = getVideo();
+
+let container = document.createElement("div");
+container.textContent = "Animator Playback!"
+
+container.style.zIndex = "999";
+container.style.position = "absolute";
+container.style.width = "70%";
+container.style.height = "10%";
+container.style.backgroundColor = "black";
+container.style.border = "2px solid #00ffaa"
+container.style.borderRadius = "5px"
+container.style.top = "20px";
+container.style.left = "20px";
+
+document.getElementById("movie_player")?.append(container);
