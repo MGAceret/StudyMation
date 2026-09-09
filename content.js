d90acc5cbedf9a6a2da9604d782381d746458ec5
MGAceret <mgaceret@gmail.com>
2026-09-09 12:46:57 +0800

    tranfer playback-hub css property to style.css

--- content.js
+++ content.js
@@ -10,23 +10,4 @@ let container = document.createElement("div");
 container.className = "playback-hud";
 container.textContent = "Animator-Playback!";
 
-
-const styleSheet = document.createElement("style");
-styleSheet.textContent = `
-    .playback-hud {
-        position: absolute;
-        top: 20px;
-        left: 20px;
-        width: 40%;
-        height: 10%;
-        background: rgba(18,18,18,0.85);
-        border: 2px solid white;
-        border-radius: 5px;
-        z-index: 999;
-        color: white;
-    }
-`
-document.head.appendChild(styleSheet);
-
-
 document.getElementById("movie_player")?.append(container);
